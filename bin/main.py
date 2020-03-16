686fb346d0727a338db06bb522fb83c730191d11
adch99 <adityachincholi@gmail.com>
2020-03-16 10:30:28 +0530

    Added all import statements

--- bin/main.py
+++ bin/main.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from scipy.signal import gaussian
+import src.preprocesser as pre
+import src.network as network
+import src.plotter as plotter
 # Getting the data
 headers = ["Year",
            "Month",
@@ -23,22 +27,22 @@ hidden_layer_size_2 = 64
 loss_func = "mse"
 
 # Data Preprocessing
-x, y = preprocess(data)
+x, y = pre.preprocess(data)
 weights = gaussian(M=mean_length, std=mean_length, sym=True)
-y = running_mean(y, weights)
-x_slid, y_slid = sliding_window_main(x, y)
-x_train, y_train, x_val, y_val, x_test, y_test = data_splitting_main(x_slid, y_slid)
+y = pre.running_mean(y, weights)
+x_slid, y_slid = pre.sliding_window_main(x, y)
+x_train, y_train, x_val, y_val, x_test, y_test = pre.data_splitting_main(x_slid, y_slid)
 
-net = create_network()
+net = network.create_network()
 
 
-history = trainer(net, x_train, y_train, x_val, y_val, verbose=1)
+history = network.trainer(net, x_train, y_train, x_val, y_val, verbose=1)
 var_train = np.var(y_train)
 var_val = np.var(y_val)
 print("Variance in y_train:", var_train)
 print("Variance in y_val:", var_val)
 
-plot_loss_vs_epoch(history)
+plotter.plot_loss_vs_epoch(history)
 
 
 # loss, val_loss = learning_curve()
