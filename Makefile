361ab9f56a7344ad749a8025a6378f78332be436
adch99 <adityachincholi@gmail.com>
2020-03-16 15:39:06 +0530

    Epochs changed to 20, Makefile added.

---
+++ Makefile
@@ -0,0 +1,6 @@
+default:
+	python bin/main.py 2> logs/errors.txt
+	less logs/errors.txt
+
+clean:
+	rm src/*.pyc
--- bin/main.py
+++ bin/main.py
@@ -29,7 +29,7 @@ x_train, y_train, x_val, y_val, x_test, y_test = pre.data_splitting_main(x_slid,
 net = network.create_network()
 
 
-history = network.trainer(net, x_train, y_train, x_val, y_val, verbose=2)
+history = network.trainer(net, x_train, y_train, x_val, y_val, verbose=1)
 var_train = np.var(y_train)
 var_val = np.var(y_val)
 print("Variance in y_train:", var_train)
